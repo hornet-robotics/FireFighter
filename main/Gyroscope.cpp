cad65a0c54d3d616b00cd20bce051d72ecc1a7b4
ki-fa <kiyarash.farsad@gmail.com>
2024-11-12 22:17:53 -0800

    comments on Gyroscope

--- main/Gyroscope.cpp
+++ main/Gyroscope.cpp
@@ -1,14 +1,16 @@
 #include "Gyroscope.h"
-//init and offset to base 130
+
+//init and offset to base 130 (no movement)
 void Gyroscope::init() {
   Serial.begin(9600);
   Wire.begin();
   mpu.initialize();
   mpu.setZGyroOffset(150);
 }
-//*getters and setters*//
+
+//Meant to be used in loop, reads gyro Z (yaw) angular velocity
 byte Gyroscope::getZ(){
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
   z = map(gz, -32768, 32768, 0, 255); 
-  return z - 129;
+  return z - 129; //negative values are clockwise
 }
--- main/Gyroscope.h
+++ main/Gyroscope.h
@@ -1,6 +1,6 @@
 #pragma once // acts as header gaurd
 
-#include <I2Cdev.h>
+#include <I2Cdev.h> //For binary address for values
 #include <Wire.h> //Wire library for MPU
 #include <MPU6050.h> //MPU6050 library for Gyroscope
 
@@ -12,7 +12,7 @@ class Gyroscope {
   byte getZ();
 
   private:
-  int16_t ax, ay, az;
+  int16_t ax, ay, az; //accel ignore if not used
   int16_t gx, gy, gz; //ignore all but gz
   MPU6050 mpu;
   byte z;
